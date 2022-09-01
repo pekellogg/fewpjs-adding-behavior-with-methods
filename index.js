eedc5f9490605282c43b4d4d1ea07dc52582bb0b
Peyton Kellogg <pekellogg@hotmail.com>
2022-08-31 19:35:59 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+class Cat {
+  constructor(name, gender_id) {
+    this.name = name;
+    this.gender_id = gender_id;
+  }
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, gender_id) {
+    this.name = name;
+    this.gender_id = gender_id;
+  }
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, gender_id) {
+    this.name = name;
+    this.gender_id = gender_id;
+  }
+  speak() {
+    if (this.gender_id == "male") {
+      return `It's me! ${this.name}, the parrot!`;
+    }
+    return `${this.name} says squawk!`;
+  }
+}
